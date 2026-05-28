e98b83d535215b0203fb182da9159e56fd84c871
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-05-28 22:05:56 +0530

    day1 task

---
+++ Day 1/question4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+//Write a program to Count digits in a number.
+int n, count = 0;
+printf("enter the number n =");
+scanf("%d",&n);
+while(n>0){
+    n = n/10;
+    count = count + 1;
+}
+printf("total number of digit is %d " , count);
+}
