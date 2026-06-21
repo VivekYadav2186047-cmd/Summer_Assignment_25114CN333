bfcb192c5254fdabcef0f104e3f01b8275985c65
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-06-21 22:45:03 +0530

    Create Question 1.py

---
+++ Day 1/Question 1.py
@@ -0,0 +1,8 @@
+#Write a program to Find string length without strlen().
+
+string = input("enter the string =")
+lenght = 0
+
+for char in string :
+    lenght += 1
+print("lenght of the string is ="  , lenght) 
