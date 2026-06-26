d9f042b018dddea84fc8306664e28ed6b06cc34a
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-06-26 09:57:21 +0530

    Add voting eligibility system based on age

---
+++ DAY  - 26/question 2.py
@@ -0,0 +1,7 @@
+#  Write a program to Create voting eligibility system.
+age = int(input("enter your age : "))
+
+if(age>=18):
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
